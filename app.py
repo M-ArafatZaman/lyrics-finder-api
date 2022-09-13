55a90b40db542d0dbaaddf42041f75fc5789166e
Mohammad Arafat Zaman <mdarafatzaman1@gmail.com>
2022-09-13 09:40:35 +0600

    Added environment variables and a secret key.

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+
+# Load Environment variables
+load_dotenv('.env')
+
+SECRET_KEY = os.getenv("APP_SECRET_KEY")
+
+# Setup server
 app = Flask(__name__)
+app.config["SECRET_KEY"] = SECRET_KEY
 
 
 @app.route('/')
 def index():
-    return "Hello world!"
+    return {"hello": "world"}
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
